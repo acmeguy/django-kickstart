5fa0434dba1f9404e04234a8fd44a9732366dafd
Stefán Baxter <stebax@gmail.com>
2012-09-22 18:05:31 +0000

    redishing

--- kickstart/local_settings.py
+++ kickstart/local_settings.py
@@ -10,12 +10,12 @@ IS_DEVELOPMENT = True
 DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'kickstart_db', # Or path to database file if using sqlite3.
-        'USER': 'kickstart', # Not used with sqlite3.
+        'ENGINE':   'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME':     'kickstart_db', # Or path to database file if using sqlite3.
+        'USER':     'kickstart', # Not used with sqlite3.
         'PASSWORD': 'kickstart', # Not used with sqlite3.
-        'HOST': 'localhost', # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+        'HOST':     'localhost', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT':     '', # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -43,6 +43,11 @@ LOCAL_CACHES = {
 # Development settings
 #
 INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    }
+
 
 DEVSERVER_MODULES = (
     'devserver.modules.sql.SQLRealTimeModule',
