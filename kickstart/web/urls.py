a68b0ebc5e45ef83270f8b805c09c35f44f2de6b
Stefán Baxter <stebax@gmail.com>
2012-09-24 14:38:00 +0000

    reference templates

--- kickstart/web/urls.py
+++ kickstart/web/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     # Signup, signin and signout
     url(r'^$', Home.as_view(template_name="web/pages/about_us.html"),name="home"),
     url(r'^about_us/', Home.as_view(template_name="web/pages/about_us.html"),name="about"),
-    url(r'^blog/(?P<slug>\w+)/$', Home.as_view(template_name="web/pages/blog_post.html"),name="blog"),
+    url(r'^blog/(?P<slug>\w+)/$', Home.as_view(template_name="web/pages/blog_post.html"),name="blogpost"),
     url(r'^blog/', Home.as_view(template_name="web/pages/blog_overview.html"),name="blog"),
     url(r'^faq/', Home.as_view(template_name="web/pages/faq_list.html"),name="faq"),
     url(r'^pricing/', Home.as_view(template_name="web/pages/pricing_overview.html"),name="pricing"),
