c9536f77ea90a2d91c7b362a11251d6235995216
Stefán Baxter <stebax@gmail.com>
2012-09-24 22:59:11 +0000

    Progress

--- kickstart/web/urls.py
+++ kickstart/web/urls.py
@@ -3,7 +3,7 @@ from views import Home
 
 urlpatterns = patterns('',
     # Signup, signin and signout
-    url(r'^$', Home.as_view(template_name="web/pages/about_us.html"),name="home"),
+    url(r'^$', Home.as_view(template_name="web/pages/index.html"),name="home"),
     url(r'^about_us/', Home.as_view(template_name="web/pages/about_us.html"),name="about"),
     url(r'^blog/(?P<slug>\w+)/$', Home.as_view(template_name="web/pages/blog_post.html"),name="blogpost"),
     url(r'^blog/', Home.as_view(template_name="web/pages/blog_overview.html"),name="blog"),
@@ -13,4 +13,5 @@ urlpatterns = patterns('',
     url(r'^testimonials/', Home.as_view(template_name="web/pages/testimonials.html"),name="testimonials"),
     url(r'^portfolio/(?P<slug>\w+)/$', Home.as_view(template_name="web/pages/portfolio_item.html"),name="portfolio"),
     url(r'^portfolio/', Home.as_view(template_name="web/pages/portfolio.html"),name="portfolio"),
+    url(r'^contact/', Home.as_view(template_name="web/pages/contact.html"),name="contact"),
 )
