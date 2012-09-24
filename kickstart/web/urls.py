55caa99d89ee39d81f8659535c75ff4326c67e29
Stefán Baxter <stebax@gmail.com>
2012-09-24 12:09:08 +0000

    local_settings and merge

--- kickstart/web/urls.py
+++ kickstart/web/urls.py
@@ -3,7 +3,7 @@ from views import Home
 
 urlpatterns = patterns('',
     # Signup, signin and signout
-    url(r'^$', Home.as_view(template_name="web/pages/about_us.html"),name="homepage"),
+    url(r'^$', Home.as_view(template_name="web/pages/about_us.html"),name="home"),
     url(r'^about_us/', Home.as_view(template_name="web/pages/about_us.html"),name="about"),
     url(r'^blog/', Home.as_view(template_name="web/pages/blog_overview.html"),name="blog"),
     url(r'^faq/', Home.as_view(template_name="web/pages/faq_list.html"),name="faq"),
